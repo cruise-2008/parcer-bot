fbf615678135b444a1a617c06ce4593f5fb91f81
Vitalii S <vitaliis@iMac-Vitalii.local>
2026-04-15 14:53:44 +0200

    config: env, requirements, procfile

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+SEARCH_INTERVAL_MINUTES = int(os.getenv("SEARCH_INTERVAL_MINUTES", 10))
